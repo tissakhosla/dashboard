f31591a0ab8e059d7d2a35f165c4ef309e5a526d
tissa <ubuntu@precision>
2026-06-03 17:52:05 -0400

    updated cloudflare worker

--- slog.js
+++ slog.js
@@ -1,4 +1,4 @@
-const WORKER_URL = 'https://YOUR-WORKER.workers.dev';
+const WORKER_URL = 'https://dashboard.tissa-music.workers.dev';
 
 async function fetchLastRow() {
     const res = await fetch(WORKER_URL);
